35575868bb0523ff58b8d1dfc4c1236e91d2b75b
Adithya Khamithkar <nkadithya31@gmail.com>
2015-11-19 08:27:36 +0530

    Fixing Jenkins shell script

--- scripts/Jenkins-ansible-shell.sh
+++ scripts/Jenkins-ansible-shell.sh
@@ -11,10 +11,10 @@ fi
 cp deployment-nginx.yml deployment-nginx-$JOB_NAME-$BUILD_NUMBER.yml
 
 echo "Deploying on $HOST_NAME"
-sudo ansible-playbook -i inventory/estateace-staging/hosts deployment-nginx-$JOB_NAME-$BUILD_NUMBER.yml -e "inventory=$HOST_NAME serial=$SERIAL" -vvvv || true
+sudo ansible-playbook -i inventory/hosts deployment-nginx-$JOB_NAME-$BUILD_NUMBER.yml -e "inventory=$HOST_NAME serial=$SERIAL" -vvvv || true
 if [ -f /root/deployment-nginx-$JOB_NAME-$BUILD_NUMBER.retry ]
 then
         echo "Deployment failed on $HOST_NAME"
-        cat ~/install-lemp-$JOB_NAME-$BUILD_NUMBERretry
+        cat ~/install-lemp-$JOB_NAME-$BUILD_NUMBER.retry
         exit 1
 fi
