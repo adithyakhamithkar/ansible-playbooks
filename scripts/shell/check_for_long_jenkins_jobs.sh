0968dd8d59d220785381e436d24c51651c307f36
Adithya Khamithkar <nkadithya31@gmail.com>
2018-02-02 12:18:37 +0530

    Updated an ignore condition

--- scripts/shell/check_for_long_jenkins_jobs.sh
+++ scripts/shell/check_for_long_jenkins_jobs.sh
@@ -1,18 +1,22 @@
 #/bin/bash
 #A script to check for long running jenkins jobs and send an email
+#dependencies 'jq'
 jenkins_url_prefix=$1
 alert_email=$2
+ignore_node=$3
 
 echo "Printing the number of jenkins nodes with executors"
 for jenkins_node_name in `curl -f --silent http://${jenkins_url_prefix}/jenkins/computer/api/json?pretty=true | jq -r .computer[].displayName`; do
   if [ "$jenkins_node_name" == "master" ]
     then
       echo "Lets skip the check for $jenkins_node_name";
-
+    elif [[ -n "`echo $jenkins_node_name | grep -e $ignore_node`"  ]];
+      then
+        echo "Lets skip the check for $jenkins_node_name";
     else
       jenkins_node_details_json="`curl -f --silent ${jenkins_url_prefix}/jenkins/computer/${jenkins_node_name}/api/json`"
       num_of_executors_1="`echo $jenkins_node_details_json | jq .numExecutors`"; num_of_executors="$(( num_of_executors_1 - 1 ))"
-      echo "$jenkins_node_name has $num_of_executors free executors"
+      echo "$jenkins_node_name has $num_of_executors executors"
       for executor_number in `seq 0 $num_of_executors`; do
         command_check='1'
         executor_details="`curl -f --silent ${jenkins_url_prefix}/jenkins/computer/${jenkins_node_name}/executors/${executor_number}/api/json`"
@@ -28,6 +32,7 @@ for jenkins_node_name in `curl -f --silent http://${jenkins_url_prefix}/jenkins/
           continue
         fi
         if [ "`echo $executor_details | jq -r .likelyStuck`" == "true" ]; then
+          echo "`echo $executor_details | jq -r .currentExecutable.url` This Jenkins job seems to be stuck. Please check. Email sent to $alert_email"
           echo "`echo $executor_details | jq -r .currentExecutable.url` This Jenkins job seems to be stuck. Please check." | mail -s "A Jenkins job seems to be stuck" $alert_email
           exit_condition="$(( exit_condition + 1 ))"
         fi
