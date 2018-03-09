e1b8634e0d066bb34e790aeb076e7aca2ca65260
Adithya Khamithkar <nkadithya31@gmail.com>
2018-03-09 14:44:31 +0530

    Updating the script

--- scripts/shell/ping-slack.sh
+++ scripts/shell/ping-slack.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 channel=$1
-token=$2
+message=$2
+username=$3
+token=$4
 
-curl -X POST --data-urlencode "payload={\"channel\": \"#$channel\", \"username\": \"webhookbot\", \"text\": \"This is posted to #general and comes from a bot named webhookbot.\", \"icon_emoji\": \":ghost:\"}'" https://hooks.slack.com/services/$token
+curl -X POST --data-urlencode "payload={\"channel\": \"#$channel\", \"username\": \"$username\", \"text\": \"$message\", \"icon_emoji\": \":ghost:\"}'" https://hooks.slack.com/services/$token
